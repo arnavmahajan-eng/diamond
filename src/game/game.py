e654ba29fa95b942a7a6dc9a657b0a4a7cd9be86
ubuntu <arnav.mahajan@xtreamcompute.com>
2026-05-28 11:19:03 +1000

    Main Dirc FPS

--- src/game/game.py
+++ src/game/game.py
@@ -140,6 +140,8 @@ class Game:
                 for j, col in enumerate(header):
                     for i, row in enumerate(col):
                         draw_text(row, idx_line=i, idx_column=j, num_cols=num_cols)
+                fps_surface = font.render(f"FPS: {clock.get_fps():.1f}", True, pygame.Color("cyan"))
+                screen.blit(fps_surface, (self.width - fps_surface.get_width() - 5, header_height - font_size - 5))
 
             pygame.display.flip()  # update screen
             clock.tick(self.fps)  # ensures game maintains the given frame rate
